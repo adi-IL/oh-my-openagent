code-yeongyu/oh-my-opencode

feat(git-master): support Git config and environment attribution overrides
Add support for suppressing AI commit footers and co-author trailers via:
- Git configuration: omo.attribution and sisyphus.attribution
- Environment variables: NO_AI_ATTRIBUTION, OMO_NO_COMMIT_ATTRIBUTION, OMO_GIT_ATTRIBUTION

This gives developers and organizations native Git control over commit history
without requiring manual edits to omo JSON configuration files.

packages/omo-senpi/src/components/git-master/index.test.ts
@@ -6,8 +6,10 @@ import { OmoGitMasterSettingsSchema, type OmoGitMasterSettings } from "@oh-my-op
 import { FakeExtensionAPI } from "../../../test-support/fake-extension-api"
 import type { ComponentContext, ComponentLogger } from "../../extension/types"
 import { createGitMasterAttributionComponent } from "./index"
+import { buildGitMasterAttributionDirective } from "./directive"
 
 const CO_AUTHOR_TRAILER = "Co-authored-by: sisyphus-dev-ai <sisyphus-dev-ai@users.noreply.github.com>"
+
 
 function createTestContext(pi: FakeExtensionAPI): ComponentContext {
   const logger: ComponentLogger = {
@@ -173,4 +175,34 @@ describe("omo-senpi git-master attribution component", () => {
     if (transform === undefined) return
     expect(appendedTextOf(transform)).toContain(CO_AUTHOR_TRAILER)
   })
+
+  it("#given git config omo.attribution false #when building directive #then directive is undefined", () => {
+    const directive = buildGitMasterAttributionDirective(defaultSettings(), {
+      env: {},
+      gitConfigReader: (key) => (key === "omo.attribution" ? "false" : undefined),
+    })
+    expect(directive).toBeUndefined()
+  })
+
+  it("#given NO_AI_ATTRIBUTION=1 #when building directive #then directive is undefined", () => {
+    const directive = buildGitMasterAttributionDirective(defaultSettings(), {
+      env: { NO_AI_ATTRIBUTION: "1" },
+      gitConfigReader: () => undefined,
+    })
+    expect(directive).toBeUndefined()
+  })
+
+  it("#given custom footer with NO_AI_ATTRIBUTION=1 #when building directive #then custom footer is kept but co-author is excluded", () => {
+    const directive = buildGitMasterAttributionDirective(
+      OmoGitMasterSettingsSchema.parse({ commit_footer: "Custom Co." }),
+      {
+        env: { NO_AI_ATTRIBUTION: "1" },
+        gitConfigReader: () => undefined,
+      },
+    )
+    expect(directive).toBeDefined()
+    expect(directive).toContain("Custom Co.")
+    expect(directive).not.toContain(CO_AUTHOR_TRAILER)
+  })
 })
+

adi-IL/oh-my-openagent

packages/omo-senpi/src/components/git-master/index.test.ts
@@ -6,8 +6,10 @@ import { OmoGitMasterSettingsSchema, type OmoGitMasterSettings } from "@oh-my-op
 import { FakeExtensionAPI } from "../../../test-support/fake-extension-api"
 import type { ComponentContext, ComponentLogger } from "../../extension/types"
 import { createGitMasterAttributionComponent } from "./index"
+import { buildGitMasterAttributionDirective } from "./directive"
 
 const CO_AUTHOR_TRAILER_PATTERN = /Co-authored-by:/i
+
 
 function createTestContext(pi: FakeExtensionAPI): ComponentContext {
   const logger: ComponentLogger = {
@@ -184,4 +186,34 @@ describe("omo-senpi git-master attribution component", () => {
     if (transform === undefined) return
     expect(appendedTextOf(transform)).toContain("Ultraworked with")
   })
+
+  it("#given git config omo.attribution false #when building directive #then directive is undefined", () => {
+    const directive = buildGitMasterAttributionDirective(defaultSettings(), {
+      env: {},
+      gitConfigReader: (key) => (key === "omo.attribution" ? "false" : undefined),
+    })
+    expect(directive).toBeUndefined()
+  })
+
+  it("#given NO_AI_ATTRIBUTION=1 #when building directive #then directive is undefined", () => {
+    const directive = buildGitMasterAttributionDirective(defaultSettings(), {
+      env: { NO_AI_ATTRIBUTION: "1" },
+      gitConfigReader: () => undefined,
+    })
+    expect(directive).toBeUndefined()
+  })
+
+  it("#given custom footer with NO_AI_ATTRIBUTION=1 #when building directive #then custom footer is kept but co-author is excluded", () => {
+    const directive = buildGitMasterAttributionDirective(
+      OmoGitMasterSettingsSchema.parse({ commit_footer: "Custom Co." }),
+      {
+        env: { NO_AI_ATTRIBUTION: "1" },
+        gitConfigReader: () => undefined,
+      },
+    )
+    expect(directive).toBeDefined()
+    expect(directive).toContain("Custom Co.")
+    expect(directive).not.toMatch(CO_AUTHOR_TRAILER_PATTERN)
+  })
 })
+